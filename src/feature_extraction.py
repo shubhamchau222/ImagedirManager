b48b1d499fe4e60a03ba4c58948576820780d9ab
unknown <shubhamchau78@gmail.com>
2021-10-30 03:03:29 +0530

    temp change

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -6,36 +6,34 @@ import pickle
 from tqdm import tqdm
 import argparse
 import os
-
-from yaml import load
+import yaml 
 from src.utils.all_utils import create_dir , genFilesnamepkl , load_config
 
-def createfilenamepkl(configPath:str):
+def createfilenamepkl(configPath:str) -> str:
     data = load_config(configPath)
     print(data)
     main_dir = data['internal_ops']['artifact_dir']
     dumpdir = data['internal_ops']['dumpeddir']
     pklname=data['internal_ops']['src_pkl_filename']
+    messeddirPath= data['internal_ops']['messed_dirPath']
     dumdirFullpath= os.path.join(main_dir,dumpdir)
     pklfilepath= os.path.join(dumdirFullpath,pklname)
-    print(pklfilepath)
+    # print(pklfilepath)
 
     # first create dir to store pkl filenames
-    # create_dir([main_dir])
-    
-    # create_dir([dumdirFullpath])
-    
+    create_dir([main_dir])  # check if artifacts dir is available ? if no then create else:pass    
+    create_dir([dumdirFullpath])  # creting dir to store pkl files 
+    messedDirPath = genFilesnamepkl(messedDirPath=messeddirPath , dumpingPath=pklfilepath) 
+    return messedDirPath
+
+  
+# model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
+
+print('Hello world')
 
 
-
-
-
-#model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
-
-# messed_up_dir => get-images/there_names => 
-# extracting the features from the img 
-def feature_extracor(img_path,model):
-    pass 
+# def feature_extracor(img_path,model):
+#     pass 
 
 
 if __name__ == '__main__':
