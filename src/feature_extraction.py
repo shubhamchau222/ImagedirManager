adedd9628c5d909e42068859389e8b1df956bdcd
unknown <shubhamchau78@gmail.com>
2021-10-30 02:13:17 +0530

    nothing changes

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -4,12 +4,17 @@ from keras_vggface.vggface import VGGFace
 import numpy as np
 import pickle
 from tqdm import tqdm
-import argparse 
+import argparse
 
 
 model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
 
+# messed_up_dir => get-images/there_names => 
 
-def feature_extracor():
+
+
+
+# extracting the features from the img 
+def feature_extracor(img_path,model):
     pass 
 
