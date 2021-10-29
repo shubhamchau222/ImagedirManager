b1720f31f4f45e2f85333fee6bc897d403841cc9
unknown <shubhamchau78@gmail.com>
2021-10-30 02:50:08 +0530

    module for creating basic dir setup for pkl data created

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -5,16 +5,43 @@ import numpy as np
 import pickle
 from tqdm import tqdm
 import argparse
+import os
+
+from yaml import load
+from src.utils.all_utils import create_dir , genFilesnamepkl , load_config
+
+def createfilenamepkl(configPath:str):
+    data = load_config(configPath)
+    print(data)
+    main_dir = data['internal_ops']['artifact_dir']
+    dumpdir = data['internal_ops']['dumpeddir']
+    pklname=data['internal_ops']['src_pkl_filename']
+    dumdirFullpath= os.path.join(main_dir,dumpdir)
+    pklfilepath= os.path.join(dumdirFullpath,pklname)
+    print(pklfilepath)
+
+    # first create dir to store pkl filenames
+    # create_dir([main_dir])
+    
+    # create_dir([dumdirFullpath])
+    
 
 
-model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
+
+
+
+#model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
 
 # messed_up_dir => get-images/there_names => 
-
-
-
-
 # extracting the features from the img 
 def feature_extracor(img_path,model):
     pass 
 
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config" ,"-c" , default="configs/structure.yaml" ) 
+    # adding the arguments , default = 'congig filepath'
+    parsed_args = args.parse_args()
+    print(parsed_args)
+    createfilenamepkl(parsed_args.config)
