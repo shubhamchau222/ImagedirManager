f7bd929e57d40573c030a63cfeac587d4cd68d59
unknown <shubhamchau78@gmail.com>
2021-11-01 09:39:01 +0530

    shell script edited2

--- setup_env.sh
+++ setup_env.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
 
-# conda create -n demo python==3.7 -y 
-# source activate demo
+conda create -n imgdmanager python==3.7 -y 
+source activate imgdmanager
 conda env list 
+# to confirm the activated env 
+
+
+# install the dependancies 
+
+pip install -r requirements.txt 
+
+pip list   # check all dependancies are installed or not 
+
+# check local packages are installed or not 
+
+
 
 
 
