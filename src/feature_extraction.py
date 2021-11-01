fefed13d34dd354e3b7a50fc04f3e8af9370e0e5
unknown <shubhamchau78@gmail.com>
2021-11-01 09:31:37 +0530

    debugging 2

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -15,20 +15,20 @@ structure_data = load_config("configs/structure.yaml" )
 model_data = None 
 
 def createfilenamepkl(config_data:dict) -> list:
+    # debuging done 
     data = config_data
     main_dir = data['internal_ops']['artifact_dir']
     dumpdir = data['internal_ops']['dumpeddir']
     pklname=data['internal_ops']['src_pkl_filename']
-    print(type(pklname))
     messeddirPath= data['internal_ops']['messed_dirPath']
     dumdirFullpath= os.path.join(main_dir,dumpdir)
     pklfilepath= os.path.join(dumdirFullpath,pklname)
-    # print(pklfilepath)
+    
     # first create dir to store pkl filenames
     create_dir([main_dir])  # check if artifacts dir is available ? if no then create else:pass    
     create_dir([dumdirFullpath])  # creting dir to store pkl files 
     messedDirPath = genFilesnamepkl(messedDirPath=messeddirPath , dumpingPath=pklfilepath) 
-    return [messedDirPath , pklfilepath]
+    return messedDirPath , pklfilepath
 
   
 vggf_model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
@@ -46,14 +46,22 @@ def get_features(img_path , model):
     result = model.predict(preprocessed_img).flatten()
     return result
 
-def feature_extractor(config_data:dict , file_names_pklfile_path:str):   # require the filename also 
+def feature_extractor(config_data:dict , messed_dir:str ,file_names_pklfile_path:str): 
+    # debugging Done 
+    # print(file_names_pklfile_path , '\n')
+    # print(messed_dir , '\n')  # require the filename also 
     config_data=config_data
     extracted_features = []
+    # file_names_pklfile_path = 'artifacts/fileNamespkl/filenames.pkl'
+    # messed_dir = './images/'
     try:
         if os.path.isfile(file_names_pklfile_path):
+            print('Path is Available....')
             filenames = pickle.load(open(file_names_pklfile_path , mode='rb'))
+            # print(filenames)
             for file in filenames:
-                feature = get_features(img_path=file , model=vggf_model)
+                file_ = os.path.join(messed_dir , file )
+                feature = get_features(img_path=file_ , model=vggf_model)
                 extracted_features.append(feature)    
 
             # save extracted features at given loc 
@@ -61,11 +69,12 @@ def feature_extractor(config_data:dict , file_names_pklfile_path:str):   # requi
             imgfeaturesdir = config_data['internal_ops']['ExtractedFeatures']['imgfeaturesdir']
             imgFeaturesFilename = config_data['internal_ops']['ExtractedFeatures']['imgFeaturesFilename']
             imgfeaturesdir_full_path = os.path.join(maindir,imgfeaturesdir)
-            Filename = os.path.join(maindir , imgfeaturesdir , imgFeaturesFilename)      # features_embedding.pkl
+            Filename = os.path.join(maindir , imgfeaturesdir , imgFeaturesFilename)     
+            # print(Filename) # features_embedding.pkl
 
             # check imgfeaturesdir is present/not else:create
             create_dir([imgfeaturesdir_full_path]) 
-            pickle.dump(extracted_features,open(Filename , 'rb'))
+            pickle.dump(extracted_features,open(Filename , 'wb'))
 
         else:
             print('Filename Pickle file not Found....')      
@@ -78,11 +87,9 @@ if __name__ == '__main__':
     args.add_argument("--configdata" ,"-d" , default=structure_data ) 
     parsed_args = args.parse_args()
     try:
-        OPFN = createfilenamepkl(parsed_args.configdata)
-        FnamePklPath = OPFN[1] 
-        print(FnamePklPath)
-        #feature_extractor(config_data=parsed_args.configdata , File_names_pklfile_path=FnamePklPath)
-        #print('Feature Extaractor Done successFully....')
+        messed_dir , file_names_pklfile_path = createfilenamepkl(parsed_args.configdata)
+        feature_extractor(config_data=parsed_args.configdata , messed_dir=messed_dir , file_names_pklfile_path = file_names_pklfile_path)
+        print('Feature Extaractor Done successFully....')
 
     except Exception as e:
         raise e 
